ec33ffa676639f65638dd4f752f56db31e5cebcc
Raoul de Heer <raouldeheer@users.noreply.github.com>
2022-03-01 22:14:45 +0100

    Added All.proto

--- src/protoparsers/protoTypes.ts
+++ src/protoparsers/protoTypes.ts
@@ -32,35 +32,31 @@ export const enum KeyValueChangeKey {
     battlefieldstatus = "battlefieldstatus",
 }
 
-const ServerInfoPackage = protobuf.loadSync("./src/protos/ServerInfo.proto");
-const WarPackage = protobuf.loadSync("./src/protos/War.proto");
-const PlayerPackage = protobuf.loadSync("./src/protos/Player.proto");
-const MissionDetailsPackage = protobuf.loadSync("./src/protos/MissionDetails.proto");
-const CommonPackage = protobuf.loadSync("./src/protos/Common.proto");
+const Protos = protobuf.loadSync("./src/protos/All.proto");
 
 export const SetProtoParsers = new Map<string, protobuf.Type>([
-    [KeyValueChangeKey.HostingCenterInfo, ServerInfoPackage.lookupType("ServerInfo.HostingCenterInfo")],
-    [KeyValueChangeKey.CommandNodeDefinition, WarPackage.lookupType("War.CommandNodeDefinition")],
-    [KeyValueChangeKey.CommandNodeWarInstance, WarPackage.lookupType("War.CommandNodeWarInstance")],
-    [KeyValueChangeKey.armyresource, WarPackage.lookupType("War.armyresource")],
-    [KeyValueChangeKey.PlayerPartnerInfo, PlayerPackage.lookupType("Player.PlayerPartnerInfo")],
-    [KeyValueChangeKey.player, PlayerPackage.lookupType("Player.player")],
-    [KeyValueChangeKey.BattleInfo, WarPackage.lookupType("War.BattleInfo")],
-    [KeyValueChangeKey.FactionResourceProduction, WarPackage.lookupType("War.FactionResourceProduction")],
-    [KeyValueChangeKey.FactionResourceConsumption, WarPackage.lookupType("War.FactionResourceConsumption")],
-    [KeyValueChangeKey.MessageRecipient, ServerInfoPackage.lookupType("ServerInfo.MessageRecipient")],
-    [KeyValueChangeKey.ignoredplayerwithname, PlayerPackage.lookupType("Player.ignoredplayerwithname")],
-    [KeyValueChangeKey.ShopWarBondItemCounter, ServerInfoPackage.lookupType("ServerInfo.ShopWarBondItemCounter")],
-    [KeyValueChangeKey.friendinfo, PlayerPackage.lookupType("Player.friendinfo")],
-    [KeyValueChangeKey.FactionResourceQueue, WarPackage.lookupType("War.FactionResourceQueue")],
-    [KeyValueChangeKey.supplylinestatus, WarPackage.lookupType("War.supplylinestatus")],
-    [KeyValueChangeKey.accesspointstatus, WarPackage.lookupType("War.accesspointstatus")],
-    // [KeyValueChangeKey.transport_commandnode_segment, WarPackage.lookupType("War.transport_commandnode_segment")],
-    // [KeyValueChangeKey.transport_commandnode_destination, WarPackage.lookupType("War.transport_commandnode_destination")],
-    [KeyValueChangeKey.air_transport, WarPackage.lookupType("War.air_transport")],
-    [KeyValueChangeKey.air_commandnode_base, WarPackage.lookupType("War.air_commandnode_base")],
-    [KeyValueChangeKey.battle, WarPackage.lookupType("War.battle")],
-    [KeyValueChangeKey.ViewHandlerPulse, ServerInfoPackage.lookupType("ServerInfo.ViewHandlerPulse")],
-    [KeyValueChangeKey.faction, WarPackage.lookupType("War.faction")],
-    [KeyValueChangeKey.battlefieldstatus, WarPackage.lookupType("War.battlefieldstatus")],
+    [KeyValueChangeKey.HostingCenterInfo, Protos.lookupType("ServerInfo.HostingCenterInfo")],
+    [KeyValueChangeKey.CommandNodeDefinition, Protos.lookupType("War.CommandNodeDefinition")],
+    [KeyValueChangeKey.CommandNodeWarInstance, Protos.lookupType("War.CommandNodeWarInstance")],
+    [KeyValueChangeKey.armyresource, Protos.lookupType("War.armyresource")],
+    [KeyValueChangeKey.PlayerPartnerInfo, Protos.lookupType("Player.PlayerPartnerInfo")],
+    [KeyValueChangeKey.player, Protos.lookupType("Player.player")],
+    [KeyValueChangeKey.BattleInfo, Protos.lookupType("War.BattleInfo")],
+    [KeyValueChangeKey.FactionResourceProduction, Protos.lookupType("War.FactionResourceProduction")],
+    [KeyValueChangeKey.FactionResourceConsumption, Protos.lookupType("War.FactionResourceConsumption")],
+    [KeyValueChangeKey.MessageRecipient, Protos.lookupType("ServerInfo.MessageRecipient")],
+    [KeyValueChangeKey.ignoredplayerwithname, Protos.lookupType("Player.ignoredplayerwithname")],
+    [KeyValueChangeKey.ShopWarBondItemCounter, Protos.lookupType("ServerInfo.ShopWarBondItemCounter")],
+    [KeyValueChangeKey.friendinfo, Protos.lookupType("Player.friendinfo")],
+    [KeyValueChangeKey.FactionResourceQueue, Protos.lookupType("War.FactionResourceQueue")],
+    [KeyValueChangeKey.supplylinestatus, Protos.lookupType("War.supplylinestatus")],
+    [KeyValueChangeKey.accesspointstatus, Protos.lookupType("War.accesspointstatus")],
+    // [KeyValueChangeKey.transport_commandnode_segment, AllPackage.lookupType("War.transport_commandnode_segment")],
+    // [KeyValueChangeKey.transport_commandnode_destination, AllPackage.lookupType("War.transport_commandnode_destination")],
+    [KeyValueChangeKey.air_transport, Protos.lookupType("War.air_transport")],
+    [KeyValueChangeKey.air_commandnode_base, Protos.lookupType("War.air_commandnode_base")],
+    [KeyValueChangeKey.battle, Protos.lookupType("War.battle")],
+    [KeyValueChangeKey.ViewHandlerPulse, Protos.lookupType("ServerInfo.ViewHandlerPulse")],
+    [KeyValueChangeKey.faction, Protos.lookupType("War.faction")],
+    [KeyValueChangeKey.battlefieldstatus, Protos.lookupType("War.battlefieldstatus")],
 ]);

bd34b4d55f6d941510bd30a6e305f37f7e48a2ef
Raoul de Heer <raouldeheer@users.noreply.github.com>
2022-03-12 20:38:55 +0100

    Saving state every 30 seconds

--- src/testCon.ts
+++ src/testCon.ts
@@ -1,8 +1,12 @@
 import { Client } from "./client";
 import { DataStore } from "./datastore";
 import { ip, port } from "./env";
+import mylas from "mylas";
 import fs from "fs";
-import { toCanvas } from "./utils";
+import { toCanvas, toCanvasColored } from "./utils";
+import BufferCursor from "./buffercursor";
+import { keys } from "./types";
+import { ProtoToString } from "./proto";
 
 
 const sleep = (delay: number) => new Promise(res => setTimeout(res, delay));
@@ -15,15 +19,27 @@ cl.once("loggedin", async () => {
     cl.sendPacketToBuffer("SubscribeHostingCenterInfoView");
     await sleep(1000);
     cl.sendPacketToBuffer("subscribebattlesview");
+    await sleep(1000);
+    cl.sendPacketToBuffer("subscribewarmaplightview");
     await sleep(2000);
-    
-    toCanvas(dataStore);
-    
+
+    setInterval(() => {
+        const date = (new Date).toISOString().replace(/[-:.]/g, "");
+        console.log(`saving to: ./saves/${date}.jsonc`);
+        mylas.json.saveS(`./saves/${date}.jsonc`, dataStore.ToObject());
+    }, 30000);
+    // toCanvas(dataStore);
+
 });
 cl.on("message", (typetext, data) => {
-    if(typetext == "KeyValueChangeSet") {
+    if (typetext == "KeyValueChangeSet") {
         dataStore.SaveData(data);
-        fs.writeFileSync("total.jsonc", dataStore.ToString(), "utf-8");
+        // fs.writeFileSync("total.jsonc", dataStore.ToString(), "utf-8");
+        // toCanvasColored(dataStore);
     }
 
+});
+cl.on("closed", () => {
+    console.log("Socket closed!");
+    process.exit(1);
 });

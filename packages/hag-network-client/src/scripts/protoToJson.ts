c110901049d5ab2599b5f1fbea3090da3f84a765
Raoul de Heer <raouldeheer@users.noreply.github.com>
2022-03-27 21:32:34 +0200

    Added protoToJson

---
+++ packages/hag-network-client/src/scripts/protoToJson.ts
@@ -0,0 +1,14 @@
+import { DataStore, loadTemplate } from "..";
+import { Json } from "mylas";
+
+async function toJsonDB(name:string) {
+    const dataStore = new DataStore;
+    await loadTemplate(dataStore, name);
+    await Json.save(`./jsondb/${name}.json`, Array.from(dataStore.GetMap().get(name)!.entries()));
+}
+
+(async () => {
+    await toJsonDB("battlefield");
+    await toJsonDB("supplyline");
+    await toJsonDB("accesspoint");
+})();

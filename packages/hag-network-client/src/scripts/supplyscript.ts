be663201e6ae4329a2fa633d7a6c49b9bffa48cc
Raoul de Heer <raouldeheer@users.noreply.github.com>
2022-04-03 22:56:16 +0200

    Fixed supplyscript

--- packages/hag-network-client/src/scripts/supplyscript.ts
+++ packages/hag-network-client/src/scripts/supplyscript.ts
@@ -3,15 +3,17 @@ const accesspoint = JSON.loadS("./jsondb/accesspoint.json");
 const battlefield = JSON.loadS("./jsondb/battlefield.json");
 const supplyline = JSON.loadS("./jsondb/supplyline.json");
 
-// TODO fix this so that it works with new jsondb without keys.
-const battlefields = new Map<string, any>(battlefield as Iterable<readonly [string, any]>);
-const supplylines = new Map<string, any>(supplyline as Iterable<readonly [string, any]>);
-const accesspoints = new Map<string, any>(accesspoint as Iterable<readonly [string, any]>);
+const battlefields = new Map;
+const supplylines = new Map;
+const accesspoints = new Map;
+
+battlefield.forEach((element: { id: string; }) => { battlefields.set(element.id, element); });
+supplylines.forEach((element: { id: string; }) => { supplyline.set(element.id, element); });
+accesspoints.forEach((element: { id: string; }) => { accesspoint.set(element.id, element); });
 
 const sups = Array.from(supplylines.keys());
 
 const result = sups.map(id => {
-    
     const sup = supplylines.get(id);
     const battlefield1 = battlefields.get(accesspoints.get(sup.accesspoint1Id).battlefield);
     const battlefield2 = battlefields.get(accesspoints.get(sup.accesspoint2Id).battlefield);
@@ -21,7 +23,7 @@ const result = sups.map(id => {
         posy1: battlefield1.posy,
         posx2: battlefield2.posx,
         posy2: battlefield2.posy
-    }]
-})
+    }];
+});
 
 JSON.saveS("./jsondb/supplylinenew.json", result);

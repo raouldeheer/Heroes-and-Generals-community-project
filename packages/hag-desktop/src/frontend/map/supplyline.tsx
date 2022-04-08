03935ab6dac9737c521c4438b526b80dd38b7bfa
Raoul de Heer <raouldeheer@users.noreply.github.com>
2022-04-08 23:08:35 +0200

    Added battles to supplylines

--- packages/hag-desktop/src/frontend/map/supplyline.tsx
+++ packages/hag-desktop/src/frontend/map/supplyline.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
-import { Line } from 'react-konva';
+import { Circle, Line } from 'react-konva';
 import { WarmapEventHandler } from "../warmapEventHandler";
+import { battle } from "./battlefieldPoint";
 
 interface supplylinestatus {
     id: string;
@@ -27,27 +28,56 @@ const Supplyline = ({
 }): JSX.Element => {
     const { posx1, posy1, posx2, posy2 } = supplyline;
     const [color, setColor] = useState("#888");
+    const [battle, setBattle] = useState<battle>(null);
 
     useEffect(() => {
         warmapEventHandler.on(`supplyline${supplyline.id}`, (data: supplylinestatus) => {
             // console.log(data.color);
             setColor(data.color);
         });
+        warmapEventHandler.on(`battlesetmapEntityId${supplyline.id}`, (data: battle) => {
+            setBattle(data);
+            warmapEventHandler.on(`battledelete${data.id}`, () => {
+                setBattle(null);
+            });
+        });
     }, []);
 
-    return <Line
-        points={[
-            posx1, 
-            posy1, 
-            posx2, 
-            posy2
-        ]}
-        stroke={color}
-        strokeWidth={8}
-        listening={false}
-        transformsEnabled={"position"}
-        perfectDrawEnabled={false}
-    />;
+    function clicked() {
+        console.log(`supplylineBattle: ${battle.id}`);
+        console.log(battle);
+
+        if (battle) {
+            warmapEventHandler.emit("BattlefieldInfoPopup_Show", {
+                battlefield: supplyline,
+                battle,
+            });
+        }
+    }
+
+    return <>
+        <Line
+            points={[
+                posx1,
+                posy1,
+                posx2,
+                posy2
+            ]}
+            stroke={color}
+            strokeWidth={8}
+            listening={false}
+            transformsEnabled={"position"}
+            perfectDrawEnabled={false}
+        />
+        {battle ? <Circle
+            key={battle.id}
+            x={posx2 + (posx1 - posx2) * Number(battle.position)}
+            y={posy2 + (posy1 - posy2) * Number(battle.position)}
+            radius={8}
+            fill="orange"
+            onClick={clicked}
+        /> : null}
+    </>;
 };
 
 export default Supplyline;

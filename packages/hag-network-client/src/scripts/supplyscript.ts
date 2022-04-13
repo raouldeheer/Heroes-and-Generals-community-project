6dfc346f98e1e65164341b5ef3823caa5ed85d9c
Raoul de Heer <raouldeheer@users.noreply.github.com>
2022-04-13 21:34:09 +0200

    Fixed supplyscript

--- packages/hag-network-client/src/scripts/supplyscript.ts
+++ packages/hag-network-client/src/scripts/supplyscript.ts
@@ -8,8 +8,8 @@ const supplylines = new Map;
 const accesspoints = new Map;
 
 battlefield.forEach((element: { id: string; }) => { battlefields.set(element.id, element); });
-supplylines.forEach((element: { id: string; }) => { supplyline.set(element.id, element); });
-accesspoints.forEach((element: { id: string; }) => { accesspoint.set(element.id, element); });
+supplyline.forEach((element: { id: string; }) => { supplylines.set(element.id, element); });
+accesspoint.forEach((element: { id: string; }) => { accesspoints.set(element.id, element); });
 
 const sups = Array.from(supplylines.keys());
 
@@ -17,13 +17,13 @@ const result = sups.map(id => {
     const sup = supplylines.get(id);
     const battlefield1 = battlefields.get(accesspoints.get(sup.accesspoint1Id).battlefield);
     const battlefield2 = battlefields.get(accesspoints.get(sup.accesspoint2Id).battlefield);
-    return [id, {
+    return {
         ...sup,
         posx1: battlefield1.posx,
         posy1: battlefield1.posy,
         posx2: battlefield2.posx,
         posy2: battlefield2.posy
-    }];
+    };
 });
 
-JSON.saveS("./jsondb/supplylinenew.json", result);
+JSON.saveS("./jsondb/supplyline.json", result);

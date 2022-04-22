f2bdaa38b85eaa196fb0d67dea28f5bec89983e0
Raoul de Heer <raouldeheer@users.noreply.github.com>
2022-04-22 20:33:18 +0200

    Updated assets

--- packages/hagcp-assets/src/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as accesspoint } from "../json/accesspoint.json";
-export { default as battlefield } from "../json/battlefield.json";
-export { default as supplyline } from "../json/supplyline.json";
-export { default as armyresourcecategory } from "../json/armyresourcecategory.json";
-export * from "./assetLoading";

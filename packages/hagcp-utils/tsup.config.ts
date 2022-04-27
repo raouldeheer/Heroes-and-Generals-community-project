a1a549f743de5b4e1728183d197dd6adf3d58d71
Raoul de Heer <raouldeheer@users.noreply.github.com>
2022-04-27 20:10:52 +0200

    hagcp-utils version 1.0.0

--- packages/hagcp-utils/tsup.config.ts
+++ packages/hagcp-utils/tsup.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "tsup";
 
 export default defineConfig(options => ({
     entry: ["src/index.ts", "src/canvas.ts"],
+    splitting: false,
     clean: true,
-    sourcemap: true,
     dts: true,
     format: ["esm", "cjs"],
     minify: !options.watch,

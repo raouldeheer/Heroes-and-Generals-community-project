4317e4c6e2377e17111d9708a89db085604a3ee0
Raoul de Heer <raouldeheer@users.noreply.github.com>
2022-04-27 19:16:35 +0200

    Cleaned up scripts

--- packages/hagcp-assets/src/scripts/protoToJson.ts
+++ packages/hagcp-assets/src/scripts/protoToJson.ts
@@ -2,15 +2,15 @@ import { DataStore } from "hagcp-utils";
 import { Json } from "mylas";
 import { loadTemplate } from "..";
 
-async function toJsonDB(name:string) {
+async function toJson(name:string) {
     const dataStore = new DataStore;
     await loadTemplate(dataStore, name);
     await Json.save(`./json/${name}.json`, Array.from(dataStore.GetMap().get(name)!.values()));
 }
 
 (async () => {
-    await toJsonDB("battlefield");
-    await toJsonDB("supplyline");
-    await toJsonDB("accesspoint");
-    await toJsonDB("armyresourcecategory");
+    await toJson("battlefield");
+    await toJson("supplyline");
+    await toJson("accesspoint");
+    await toJson("armyresourcecategory");
 })();

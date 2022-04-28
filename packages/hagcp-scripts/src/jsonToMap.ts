5d3d7aeaadf595920a69225dd9e30777877d3c3b
Raoul de Heer <raouldeheer@users.noreply.github.com>
2022-04-28 18:07:14 +0200

    Added .protodata loading

--- packages/hagcp-scripts/src/jsonToMap.ts
+++ packages/hagcp-scripts/src/jsonToMap.ts
@@ -1,18 +1,24 @@
 import mylas from "mylas";
-import { DataStore } from "hagcp-utils";
+import { BufferCursor, DataStore, IKeyValueChangeSetResult } from "hagcp-utils";
 import { drawToCanvas, toCanvasColored } from "hagcp-utils/canvas";
-import { readdir } from "fs/promises";
 import { existsSync, createWriteStream } from "fs";
 import { loadTemplate } from "hagcp-assets";
 import { pipeline } from "stream/promises";
+import { gunzipSync } from "zlib";
+import { keyToClass } from "hagcp-network-client";
 
 async function jsonToMap(filename: string, imageName: string, dataStore: DataStore) {
     const { factions, ...data } = await mylas.json.load(filename);
     const dataStore2 = new DataStore;
 
-    for (const key in data) if (data.hasOwnProperty(key))
-        for (const key2 in data[key]) if (data[key].hasOwnProperty(key2))
-            dataStore2.SaveData({ set: [{ key, value: data[key][key2] }] });
+    if (existsSync(filename.replace(".jsonc", ".protodata"))) {
+        const buf = gunzipSync(await mylas.buf.load(filename.replace(".jsonc", ".protodata")));
+        dataStore2.SaveData(keyToClass.get("KeyValueChangeSet")?.parse(new BufferCursor(buf)) as IKeyValueChangeSetResult);
+    } else {
+        for (const key in data) if (data.hasOwnProperty(key))
+            for (const key2 in data[key]) if (data[key].hasOwnProperty(key2))
+                dataStore2.SaveData({ set: [{ key, value: data[key][key2] }] });
+    }
 
     if (factions) {
         const lookupFactions = new Map<string, any>();
@@ -28,6 +34,7 @@ async function jsonToMap(filename: string, imageName: string, dataStore: DataSto
 
 (async () => {
     const dataStore = new DataStore;
+    const { globby } = await import("globby");
 
     await loadTemplate(dataStore, "battlefield");
     await loadTemplate(dataStore, "supplyline");
@@ -39,10 +46,10 @@ async function jsonToMap(filename: string, imageName: string, dataStore: DataSto
     if (!warId) return;
     if (!existsSync(`./saves/${warId}`)) mylas.dir.mkS(`./saves/${warId}`);
     if (!existsSync(`./savesMap/${warId}`)) mylas.dir.mkS(`./savesMap/${warId}`);
-    const files = await readdir(`./saves/${warId}`);
+    const files = await globby(`./saves/${warId}/*.jsonc`);
     for (let i = 0; i < files.length; i++) {
         const imageName = `./savesMap/${warId}/image_${(i + 1).toString().padStart(5, "0")}.jpg`;
-        await jsonToMap(`./saves/${warId}/${files[i]}`, imageName, dataStore);
+        await jsonToMap(files[i], imageName, dataStore);
         console.log(`saved: ${imageName} (${i + 1}/${files.length})`);
     }
     console.log("Done");

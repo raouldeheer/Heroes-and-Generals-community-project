43bab9a888bc40666222312e476d87f91f4c7252
Raoul de Heer <raouldeheer@users.noreply.github.com>
2022-04-30 15:18:08 +0200

    Added playerdetail endpoint

--- packages/hagcp-warmap-screenshot/src/api.ts
+++ packages/hagcp-warmap-screenshot/src/api.ts
@@ -210,5 +210,37 @@ export async function startAPI(datastore: DataStore, client: Client, lookupFacti
         res.sendStatus(412);
     });
 
+    app.get("/playerdetail", async (req, res) => {
+        if (!client) res.sendStatus(500);
+        res.set("Cache-control", "public, max-age=60");
+        if (req.query.id) {
+            const id = String(req.query.id);
+            if (/^\d+$/.test(id)) {
+                const gamertag = (await client.sendPacketAsync<{ gamertag: string; }>(ClassKeys.QueryGamertagRequest, {
+                    playerId: Long.fromString(id)
+                })).gamertag;
+                if (gamertag) {
+                    res.json(await client.sendPacketAsync(ClassKeys.SearchPlayerDetailRequest, {
+                        playerGamerTag: gamertag
+                    }));
+                    return;
+                }
+                res.sendStatus(404);
+                return;
+            }
+        } else if (req.query.gamertag) {
+            const gamertag = String(req.query.gamertag);
+            if (/[@*\\%\s]/.test(gamertag) || gamertag.length < 4 || gamertag.length > 26 || gamertag.includes("reto.")) {
+                res.sendStatus(418); //! This should never happen!
+                return;
+            }
+            res.json(await client.sendPacketAsync(ClassKeys.SearchPlayerDetailRequest, {
+                playerGamerTag: gamertag
+            }));
+            return;
+        }
+        res.sendStatus(412);
+    });
+
     return app;
 }

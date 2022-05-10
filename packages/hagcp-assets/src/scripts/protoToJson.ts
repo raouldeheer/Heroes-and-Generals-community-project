3f12437dfd9476530b7c448c1620fd51d35bbcb4
Raoul de Heer <raouldeheer@users.noreply.github.com>
2022-05-10 19:38:48 +0200

    Update protoToJson.ts

--- packages/hagcp-assets/src/scripts/protoToJson.ts
+++ packages/hagcp-assets/src/scripts/protoToJson.ts
@@ -7,7 +7,7 @@ import { join } from "path";
 async function toJson(name:string) {
     const dataStore = new DataStore;
     await loadTemplate(dataStore, name);
-    await Json.save(`./json/${name}.json`, Array.from(dataStore.GetMap().get(name)!.values()));
+    await Json.save(`./json/${name}.json`, Array.from(dataStore.GetItemStore(name)!.values()));
 }
 
 (async () => {

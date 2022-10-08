44bab9eb3ff2c436b1c0365386d9193aff59fbb6
Raoul de Heer <raouldeheer@users.noreply.github.com>
2022-10-08 21:16:43 +0200

    Fixed long import

--- packages/hagcp-network-client/src/interfaces/statrequests.interfaces.ts
+++ packages/hagcp-network-client/src/interfaces/statrequests.interfaces.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-empty-interface */
+import Long from "long";
 import { ResponseType } from "../protolinking/classKeys";
 
 export interface AddAssaultTeamStats {

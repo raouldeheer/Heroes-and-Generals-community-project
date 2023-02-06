4e2a5844a3a3135980d463336b696a69b49ffc55
Raoul de Heer <raouldeheer@users.noreply.github.com>
2023-02-06 21:18:48 +0100

    Added hgmap parsing to jsonToCSV

--- packages/hagcp-scripts/src/jsonToCSV.ts
+++ packages/hagcp-scripts/src/jsonToCSV.ts
@@ -3,32 +3,17 @@ import { existsSync } from "fs";
 import globby from "globby";
 // @ts-expect-error fake ts(1471) error
 import { pLimit } from 'plimit-lit';
+import { parseHGMap } from "./hgmap";
+import { gunzipSync } from "zlib";
 
 const fileLimit = pLimit(100);
-const openFile = (file: string): Promise<SaveData> => fileLimit((name: string) => mylas.json.load<SaveData>(name), file);
-
-const idToAbbr = new Map<string, FactionName>([
-    ["1", "US"],
-    ["2", "GE"],
-    ["3", "USSR"],
-]);
-
-const FactionNameList: FactionName[] = ["US", "GE", "USSR"];
-const OutputOptionsList: OutputOptions[] = ["victoryPoints", "playerCount", "deployedCommandPointsInfantry", "deployedCommandPointsArmor", "deployedCommandPointsAir", "controlledBattlefields", "battlesWon", "battlesLost", "infantryLost", "vehiclesLost", "tanksLost", "planesLost"];
-
-(async () => {
-    const warId = process.argv[2];
-    console.log(`Loading ${warId}`);
-    if (!warId) return;
-    if (!existsSync(`./saves/${warId}`)) mylas.dir.mkS(`./saves/${warId}`);
-
-    const files = await globby(`./saves/${warId}/*.jsonc`);
-    if (files) {
-        const data = await Promise.all(files.map(openFile));
-        const outputData = data
-            .filter(v => v.factions?.length === 3)
-            .map(savedData => ({
-                factions: savedData.factions
+const openFile = (file: string) => fileLimit(
+    async (name: string) => {
+        if (name.endsWith(".jsonc")) {
+            const data = await mylas.json.load<SaveData>(name);
+            if (data?.factions?.length !== 3) return null;
+            return {
+                factions: data.factions
                     .sort((a, b) => Number(a.factionTemplateId) - Number(b.factionTemplateId))
                     .map<FactionOutput>(raw => ({
                         id: raw.factionTemplateId,
@@ -45,9 +30,58 @@ const OutputOptionsList: OutputOptions[] = ["victoryPoints", "playerCount", "dep
                         tanksLost: raw.tanksLost,
                         planesLost: raw.planesLost,
                     })),
-                onlineplayers: savedData?.queryServerInfo?.onlineplayers ?? 0,
-            }))
+                onlineplayers: data?.queryServerInfo?.onlineplayers ?? 0,
+            };
+        } else if (name.endsWith(".hgmap")) {
+            const data = parseHGMap(gunzipSync(await mylas.buf.load(file)));
+            if (!data?.factions) return null;
+            const factions = data.factions
+                .sort((a, b) => Number(a.factionTemplateId) - Number(b.factionTemplateId))
+                .map<FactionOutput>(raw => ({
+                    id: raw.factionTemplateId,
+                    victoryPoints: raw.factionVictoryPoints,
+                    playerCount: raw.factionPlayerCount,
+                    deployedCommandPointsInfantry: raw.factionDeployedCommandPointsInfantry,
+                    deployedCommandPointsArmor: raw.factionDeployedCommandPointsArmor,
+                    deployedCommandPointsAir: raw.factionDeployedCommandPointsAir,
+                    controlledBattlefields: raw.factionControlledBattlefields,
+                    battlesWon: raw.battlesWon,
+                    battlesLost: raw.battlesLost,
+                    infantryLost: raw.infantryLost,
+                    vehiclesLost: raw.vehiclesLost,
+                    tanksLost: raw.tanksLost,
+                    planesLost: raw.planesLost,
+                } as const));
+            return {
+                factions,
+                onlineplayers: data?.serverInfo?.onlineplayers ?? 0,
+            };
+        }
+        return null;
+    }, file);
+
+const idToAbbr = new Map<string, FactionName>([
+    ["1", "US"],
+    ["2", "GE"],
+    ["3", "USSR"],
+]);
+
+const FactionNameList: FactionName[] = ["US", "GE", "USSR"];
+const OutputOptionsList: OutputOptions[] = ["victoryPoints", "playerCount", "deployedCommandPointsInfantry", "deployedCommandPointsArmor", "deployedCommandPointsAir", "controlledBattlefields", "battlesWon", "battlesLost", "infantryLost", "vehiclesLost", "tanksLost", "planesLost"];
+
+(async () => {
+    const warId = process.argv[2];
+    console.log(`Loading ${warId}`);
+    if (!warId) return;
+    if (!existsSync(`./saves/${warId}`)) mylas.dir.mkS(`./saves/${warId}`);
+
+    const files = await globby(`./saves/${warId}/*.{hgmap,jsonc}`);
+    if (files) {
+        const data = await Promise.all(files.map(openFile));
+        const outputData = data
+            .filter(<T>(v: T): v is NonNullable<T> => !!v)
             .map(data => {
+                
                 // @ts-expect-error key
                 const result: { [key: OutputFactionOptions]: never; } = {};
                 data.factions.forEach(faction => {
